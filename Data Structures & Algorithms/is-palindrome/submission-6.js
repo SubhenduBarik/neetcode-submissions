504cd03f071b99fb45165a88c3a7ff3e20115060
Subhendu Prakash Barik <32711772+SubhenduBarik@users.noreply.github.com>
2026-09-06 01:09:39 +0530

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let i = 0;
+        let j = s.length - 1;
+        while (i < j) {
+            while (/[A-Za-z0-9]/.test(s[i]) == false && i < j) i++;
+            while (/[A-Za-z0-9]/.test(s[j]) == false && i < j) j--;
+            if(i == j) return true
+            console.log(s[i], s[j], i , j)
+            if (s[i].toLowerCase() != s[j].toLowerCase()) return false
+            i++;
+            j--;
+        }
+        return true
+    }
+}
